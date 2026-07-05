2467c541103da7843967e365044892224f7a9fb0
Arjun Kumar Yadav <kumararjunyadav9326@gmail.com>
2026-07-05 23:37:48 +0530

    Added more column in  Employee data analysis

--- 03_Pandas/Employee_Salary_Analysis/employee_analysis.py
+++ 03_Pandas/Employee_Salary_Analysis/employee_analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 df = pd.read_csv('../../datasets/employee/employee.csv')
 # print(df.head(10))
 # print(df.tail())
@@ -30,6 +30,48 @@ avg_age = df['Age'].mean().round()
 avg_exp = df["Experience"].mean()
 # print(avg_exp)
 
+#filtering
+# print("Salary greater then 70,000\n" ,df[df['Salary']>70000])
+# print("Salary Less then 60,000")
+# print(df[df['Salary']<60000])
+# print(df[df["Experience"]>5])
+# print(df[df['Age']<30])
+# print(df[df['Department']=='IT'])
+# print(df[df['City']=='Mumbai'])
+# print(df[(df['City']=='Delhi') & (df['Salary']>60000)])
+# print(df[(df['Experience']>5)&(df['Experience']<10)])
+# print(df.groupby('Department')['Salary'].mean().round(3))
+# print(df.groupby('Department')['Salary'].max())
+# print(df.groupby('Department')['Salary'].min())
+# print(df.groupby('Department')['Experience'].mean().round())
+# print(df.groupby('Department')['Name'].count()) 
+# print(df.groupby('City')["Salary"].mean().round(2))
+# print(df.groupby('City')['Name'].count()) 
+# print(df[df['Salary'] == df.groupby('Department')['Salary'].transform('max')])
+# print(df[df['Salary'] == df.groupby('Department')['Salary'].transform('min')])
+avg_salaries_by_dept =df.groupby('Department' , as_index=False)["Salary"].mean().round(2)
+highest_avg_dept =avg_salaries_by_dept.sort_values(by="Salary" ,ascending=False).iloc[0]
+# print(highest_avg_dept)
+
+#create new column
+df['Bonus'] = df['Salary']*0.1
+df["Tax"] = df['Salary']*0.05 
+df["Net Salary"] =df["Salary"]+df["Bonus"]-df["Tax"]
+level_conditions = [
+    df["Experience"] < 4,
+    (df["Experience"] >= 4) & (df["Experience"] < 8),
+    df["Experience"] >= 8,
+]
+level_labels = ["Junior", "Mid", "Senior"]
+df["Level"] = np.select(level_conditions, level_labels, default="Unknown")
+
+age_bins = [0, 29, 54, 100]
+age_labels = ["Young", "Adult", "Senior"]
 
 
-# print(df)
+df["Age Group"] = pd.cut(df["Age"], bins=age_bins, labels=age_labels)
+
+
+ 
+
+print(df)
